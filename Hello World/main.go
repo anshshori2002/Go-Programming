66ea8b4ff65fbe0fb82db340b6353d3813e698ba
ANSH SHORI <84807307+anshshori2002@users.noreply.github.com>
2023-12-15 08:38:24 +0530

    Update main.go

--- Hello World/main.go
+++ Hello World/main.go
@@ -1,1 +1,14 @@
+package main
 
+import "fmt"
+
+func main() {
+
+	var username string = "Ansh"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n",username)
+
+	var isloggedIn bool = true
+	fmt.Println(isloggedIn)
+	fmt.Printf("Variable is of type: %T \n",username)
+}
